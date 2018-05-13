3d74d27f005ae1f12368e37fc2998a0a4331250c
imacoolgirlyo <tjrud1438@naver.com>
2018-05-13 22:53:49 +0900

    Report Component(props) done

--- src/App.js
+++ src/App.js
@@ -46,13 +46,97 @@ const categories = [
       { id: 2, date: "18.02.12", name: "오피스 디포 결제", cost: "-30,000" }
     ]
   },
-  { category: "연구과제추진비", detail: "식대", balance: "200,000" },
-  { category: "연구과제추진비", detail: "회의비", balance: "880,000" },
-  { category: "연구활동비", detail: "국외여비", balance: "880,000" },
-  { category: "연구활동비", detail: "논문개재료", balance: "120,000" },
-  { category: "연구활동비", detail: "학회참가비", balance: "30,000" },
-  { category: "연구장비재료비", detail: "재료비", balance: "0" },
-  { category: "연구수당", detail: "연구수당", balance: "0" }
+  {
+    category: "연구과제추진비",
+    detail: "식대",
+    balance: "200,000",
+    usages: [
+      {
+        id: 1,
+        date: "",
+        name: "",
+        cost: ""
+      }
+    ]
+  },
+  {
+    category: "연구과제추진비",
+    detail: "회의비",
+    balance: "880,000",
+    usages: [
+      {
+        id: 1,
+        date: "",
+        name: "",
+        cost: ""
+      }
+    ]
+  },
+  {
+    category: "연구활동비",
+    detail: "국외여비",
+    balance: "880,000",
+    usages: [
+      {
+        id: 1,
+        date: "",
+        name: "",
+        cost: ""
+      }
+    ]
+  },
+  {
+    category: "연구활동비",
+    detail: "논문개재료",
+    balance: "120,000",
+    usages: [
+      {
+        id: 1,
+        date: "",
+        name: "",
+        cost: ""
+      }
+    ]
+  },
+  {
+    category: "연구활동비",
+    detail: "학회참가비",
+    balance: "30,000",
+    usages: [
+      {
+        id: 1,
+        date: "",
+        name: "",
+        cost: ""
+      }
+    ]
+  },
+  {
+    category: "연구장비재료비",
+    detail: "재료비",
+    balance: "0",
+    usages: [
+      {
+        id: 1,
+        date: "",
+        name: "",
+        cost: ""
+      }
+    ]
+  },
+  {
+    category: "연구수당",
+    detail: "연구수당",
+    balance: "0",
+    usages: [
+      {
+        id: 1,
+        date: "",
+        name: "",
+        cost: ""
+      }
+    ]
+  }
 ];
 
 export default App;
--- src/Report.js
+++ src/Report.js
@@ -11,6 +11,24 @@ class Report extends Component {
             category => category.category === "연구과제추진비"
           )}
         />
+        <ReportCategory
+          title="연구활동비"
+          categories={this.props.categories.filter(
+            category => category.category === "연구활동비"
+          )}
+        />
+        <ReportCategory
+          title="연구장비재료비"
+          categories={this.props.categories.filter(
+            category => category.category === "연구장비재료비"
+          )}
+        />
+        <ReportCategory
+          title="연구수당"
+          categories={this.props.categories.filter(
+            category => category.category === "연구수당"
+          )}
+        />
       </div>
     );
   }
@@ -18,7 +36,6 @@ class Report extends Component {
 
 class ReportCategory extends Component {
   render() {
-    console.log("ReportCatecory", this.props);
     var details = this.props.categories.map(detail => {
       return <ReportCategoryRow title={detail.detail} usages={detail.usages} />;
     });
@@ -34,8 +51,33 @@ class ReportCategory extends Component {
 class ReportCategoryRow extends Component {
   render() {
     console.log(this.props.usages);
-    return <div>{this.props.title}</div>;
+    var usages = this.props.usages.map(usage => {
+      return (
+        <CategoryRowDetail
+          date={usage.date}
+          name={usage.name}
+          cost={usage.cost}
+        />
+      );
+    });
+    return (
+      <div>
+        {this.props.title}
+        {usages}
+      </div>
+    );
   }
 }
 
+class CategoryRowDetail extends Component {
+  render() {
+    return (
+      <div>
+        <span> {this.props.date}</span>
+        <span> {this.props.name}</span>
+        <span> {this.props.cost}</span>
+      </div>
+    );
+  }
+}
 export default Report;
